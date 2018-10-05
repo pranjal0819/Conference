5045ddf5dcddaccc91b86c30d5bda8655dcdee89
pranjal0819 <pranjal0819@gmail.com>
2018-10-06 03:24:35 +0530

    66 commit

--- AbesConference/AbesConference/views.py
+++ AbesConference/AbesConference/views.py
@@ -9,17 +9,5 @@ def home(request):
     return render(request, "index.html", {'conference_list': conference_list})
 
 
-def registration(request):
-    return render(request, 'registration.html')
-
-
-def call_for_paper(request):
-    return render(request, 'call-for-paper.html')
-
-
-def proceeding(request):
-    return render(request, 'proceeding.html')
-
-
 def media(request, slug, pk):
     raise Http404
